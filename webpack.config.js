f64c2d4f9c9cbece2e85427c86720c61e5ea6d71
Karl Schmaltz <karlschmaltz@gmail.com>
2021-11-15 21:43:39 -0600

    feat: add deps from webpack not package json merge

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,43 @@ module.exports = (env) => {
         });
     }
 
+    if (fs.existsSync('../demo-snippets/App_Resources/Android')) {
+        webpack.Utils.addCopyRule({
+            from: '../demo-snippets/App_Resources/Android',
+            to: webpack.Utils.project.getProjectRootPath() + '/App_Resources'
+        });
+    }
+
+    if (fs.existsSync('../demo-snippets/App_Resources/iOS')) {
+        webpack.Utils.addCopyRule({
+            from: '../demo-snippets/App_Resources/iOS',
+            to: webpack.Utils.project.getProjectRootPath() + '/App_Resources'
+        });
+    }
+
     webpack.init(env);
+
+    const projectPath = webpack.Utils.project.getProjectRootPath();
+    const packagesPath = projectPath.substring(0, projectPath.lastIndexOf('/')) + '/packages';
+
+    fs.readdirSync(packagesPath).forEach(file => {
+        const package = require(`${packagesPath}/${file}/package.json`);
+
+        webpack.Utils.addCopyRule({
+            from: `${packagesPath}/${file}`,
+            to: package.name
+        })
+
+        if ('dependencies' in package) {
+            for (const dependency of Object.keys(package.dependencies)) {
+                console.log(`Copying dependency "${dependency}" to demo`)
+                webpack.Utils.addCopyRule({
+                    from: `${projectPath.substring(0, projectPath.lastIndexOf('/'))}/node_modules/${dependency}`,
+                    to: dependency
+                })
+            }
+        }
+    });
 
     const { redirect } = env;
 
