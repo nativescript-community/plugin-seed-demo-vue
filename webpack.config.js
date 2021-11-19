606951b5fd7b20dd9f2455b8ed404b12d6050ce0
Martin Guillon <martin.guillon@akylas.fr>
2021-11-19 09:49:06 +0100

    fix: ensure we find demo snippets deps

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,7 @@ module.exports = (env) => {
     const { redirect } = env;
 
     webpack.chainWebpack((config) => {
+        config.resolve.modules.add(resolve(__dirname, '../demo-snippets/node_modules'));
         config.plugin('DefinePlugin').tap((args) => {
             if (redirect) {
                 Object.assign(args[0], {
